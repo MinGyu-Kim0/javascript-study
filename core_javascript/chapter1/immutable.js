fc312c3390c17e2b09e7cd2db3d5135908942266
min gyu kim <coinmagic8085@gmail.com>
2026-01-31 20:04:00 +0900

    객체의 가변성에 따른 문제점 해결 방법 1/2

--- core_javascript/chapter1/immutable.js
+++ core_javascript/chapter1/immutable.js
@@ -1,4 +1,3 @@
-/*
 var user = {
   name: "Jaenam",
   gender: "male",
@@ -17,20 +16,22 @@ if (user !== user2) {
   console.log("유저 정보가 변경되었습니다.");
 }
 
-console.log(user.name, user2.name); -> Jung Jung
-console.log(user === user2); -> true
-*/
+console.log(user.name, user2.name); // -> Jung Jung
+console.log(user === user2); // -> true
 
+// 객체의 가변성에 따른 문제 해결 방법
 var user = {
   name: "Jaenam",
   gender: "male",
 };
 
-// 객체의 가변성에 따른 문제 해결 방법
 var changeName = function (user, newName) {
+  // 새로운 객체를 반환하도록 수정
   return {
     name: newName,
     gender: user.gender,
+    // 문제점 -> 변경할 필요가 없는 프로퍼티를 하드코딩으로 입력
+    // 변경해야할 정보가 많을 수록 입력하는 수고가 늘어난다.
   };
 };
 
@@ -40,5 +41,5 @@ if (user !== user2) {
   console.log("유저 정보가 변경되었습니다.");
 }
 
-console.log(user.name, user2.name);
-console.log(user === user2);
+console.log(user.name, user2.name); // -> Jaenam Jung
+console.log(user === user2); // -> false
