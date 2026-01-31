28b49f57000dd1f28134819df1f612770ee7d6a3
min gyu kim <coinmagic8085@gmail.com>
2026-02-01 05:18:41 +0900

    bind 메서드

---
+++ core_javascript/chapter3/bindMethod.js
@@ -0,0 +1,47 @@
+// this 지정과 부분 적용 함수 구현
+var func = function (a, b, c, d) {
+  console.log(this, a, b, c, d);
+};
+func(1, 2, 3, 4); // Window{ ... } 1 2 3 4
+
+var bindFunc1 = func.bind({ x: 1 });
+bindFunc1(5, 6, 7, 8); // { x: 1 } 5 6 7 8
+
+var bindFunc2 = func.bind({ x: 1 }, 4, 5);
+bindFunc2(6, 7); // { x: 1 } 4 5 6 7
+bindFunc2(8, 9); // { x: 1 } 4 5 8 9
+
+// name 프로퍼티
+var func = function (a, b, c, d) {
+  console.log(this, a, b, c, d);
+};
+var bindFunc = func.bind({ x: 1 }, 4, 5);
+console.log(func.name); // func
+console.log(bindFunc.name); // bound func
+
+// 내부함수에 this 전달
+var obj = {
+  outer: function () {
+    console.log(this);
+    var innerFunc = function () {
+      console.log(this);
+    }.bind(this);
+    innerFunc();
+  },
+};
+obj.outer();
+
+// 내부함수에 this 전달 2
+var obj = {
+  logThis: function () {
+    console.log(this);
+  },
+  logThisLater1: function () {
+    setTimeout(this.logThis, 500);
+  },
+  logThisLater2: function () {
+    setTimeout(this.logThis.bind(this), 1000);
+  },
+};
+obj.logThisLater1(); // Window { ... }
+obj.logThisLater2(); // obj { logThis: f, ... }
