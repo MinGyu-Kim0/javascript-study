f85086459618941825e75fb94f40cc14e4e041b6
min gyu kim <coinmagic8085@gmail.com>
2026-02-01 05:23:38 +0900

    화살표 함수에서의 this

---
+++ core_javascript/chapter3/arrowfunctionThis.js
@@ -0,0 +1,11 @@
+// 화살표 함수 내부에서의 this
+var obj = {
+  outer: function () {
+    console.log(this);
+    var innerFunc = () => {
+      console.log(this);
+    };
+    innerFunc();
+  },
+};
+obj.outer();
