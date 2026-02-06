529dca52930b020f8ee7bc3d08c007453c1188e0
MINGYU KIM <coinmagic8085@gmail.com>
2026-02-06 16:29:47 +0900

    콜백 함수에서의 this의 활용과 중요성

--- core_javascript/chapter4/callbackThisLegacy.js
+++ core_javascript/chapter4/callbackThisLegacy.js
@@ -9,3 +9,18 @@ var obj1 = {
 };
 var callback = obj1.func();
 setTimeout(callback, 1000);
+
+// func 함수 재활용
+var callback = obj1.func();
+setTimeout(callback, 1000);
+
+var obj2 = {
+name: 'ojb2',
+func: obj1.func
+};
+var callback2 = obj2.func();
+setTimeout(callback2, 1500);
+
+var obj3 = { name: 'obj3' };
+var callback3 = obj1.func.call(obj3);
+setTimeout(callback3, 2000);
