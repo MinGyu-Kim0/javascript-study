51c0b3538e9a7fa2f46b510ecded94efb7a8c686
MINGYU KIM <coinmagic8085@gmail.com>
2026-02-06 16:04:09 +0900

    메서드를 콜백 함수로 전달

--- core_javascript/chapter4/methodAsCallback.js
+++ core_javascript/chapter4/methodAsCallback.js
@@ -8,3 +8,5 @@ obj.logValues(1, 2);                // { vals: [1, 2, 3], logValues: f } 1 2
 [4, 5, 6].forEach(obj.logValues);   // Window { ... } 4 0
                                     // Window { ... } 5 1
                                     // Window { ... } 6 2
+
+                                    
